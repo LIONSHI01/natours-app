7bf0bf6e3ecee4ea559ad8c048cacac66342c599
LIONSHI01 <shizijian2010@yahoo.com.hk>
2022-06-20 22:06:41 +0800

    add respond to URL parameters

--- app.js
+++ app.js
@@ -18,9 +18,17 @@ app.get('/api/v1/tours', (req, res) => {
     .json({ status: 'success', result: tours.length, data: { tours } });
 });
 
-app.post('/api/v1/tours', (req, res) => {
-  // console.log(req.body);
+// Respond to URL Parameters
+app.get('/api/v1/tours/:id', (req, res) => {
+  // NOTE: convert string to integer
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (tour) res.status(200).json({ status: 'success', data: { tour } });
+  if (!tour) res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+});
 
+app.post('/api/v1/tours', (req, res) => {
+  // NOTE: newId: use the latest tour id + 1 (since some tour may be deleted, so total tour number != id number)
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
