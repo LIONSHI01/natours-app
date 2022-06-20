fd87402b357ae02a95943691aca09bc0ac88c90b
LIONSHI01 <shizijian2010@yahoo.com.hk>
2022-06-20 17:28:23 +0800

    add routing /api/v1/tours GET

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
 // set up root url
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
-
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+    .json({ status: 'success', result: tours.length, data: { tours } });
 });
 
 const port = 3000;
