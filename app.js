5bd6486c08416248e263b5f96c7cd27b1d7b34ba
LIONSHI01 <shizijian2010@yahoo.com.hk>
2022-06-20 18:04:14 +0800

    add POST /api/v1/tours

--- app.js
+++ app.js
@@ -3,25 +3,39 @@ const express = require('express');
 
 const app = express();
 
-// set up root url
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side!', app: 'Natours' });
-// });
+// KEYNOTE: use Middleware to read POST JSON data from Clients
+app.use(express.json());
 
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint...');
-// });
-
+//KEYNOTE Preload(synchronously->readFileSync()) JSON file into Javascript format with JSON.parse()
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// NOTE: specify v1 for later update to v2, client could still use v1
 app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
     .json({ status: 'success', result: tours.length, data: { tours } });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      // NOTE: status = 201 => means Created new status
+      res.status(201).json({
+        status: 'success',
+        data: { tour: newTour },
+      });
+    }
+  );
 });
 
 const port = 3000;
