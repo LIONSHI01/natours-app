0203dbdc7b317871229c3e933927b1e100d564fb
LIONSHI01 <shizijian2010@yahoo.com.hk>
2022-06-21 16:35:23 +0800

    add eslint dependencies

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
-const tourRouter = require('./routes/tourRoutes.js');
-const userRouter = require('./routes/userRoutes.js');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
