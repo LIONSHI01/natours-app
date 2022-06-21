ba65aef240f451a80e0f1a56894dedc06b298f1f
LIONSHI01 <shizijian2010@yahoo.com.hk>
2022-06-21 13:47:54 +0800

    add checkBody middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,12 +4,14 @@ const router = express.Router();
 const tourController = require('./../controllers/tourController');
 
 router.param('id', tourController.checkID);
+router.param('name', tourController.checkBody);
+// router.param('id', tourController.checkBody);
 
 // NOTE: specify v1 for later update to v2, client could still use v1
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
 // Respond to URL Parameters
 router
   .route('/:id')
