5c0f874d92fccd87fe0c530bb887ffe3785b10fb
LIONSHI01 <shizijian2010@yahoo.com.hk>
2022-06-22 09:18:13 +0800

    add test data to mongo

--- server.js
+++ server.js
@@ -35,6 +35,21 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+  name: 'The Park Camper',
+  rating: 4.6,
+  price: 490,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(`ERROR🥶`, err);
+  });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
