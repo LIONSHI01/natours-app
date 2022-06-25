be4e7ed4c7bb89aacc85ed9e856da5cf3661ae0b
LIONSHI01 <shizijian2010@yahoo.com.hk>
2022-06-25 15:27:25 +0800

    add handler to unhandler promise rejection globally

--- server.js
+++ server.js
@@ -15,10 +15,21 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+// Catch unhandled Promise Rejection GLOBALLY!! Final safe net.
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION,🥶, Shutting down...');
+  // Close the server first then the app, so let the server process all remaining requests in real world
+  server.close(() => {
+    process.exit(1); // 1 = Uncaught Exception , 0 = Success
+  });
 });
