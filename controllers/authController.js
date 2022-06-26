5be15038ba14271e63dec7ca272bda3c1a1d855d
LIONSHI01 <shizijian2010@yahoo.com.hk>
2022-06-26 09:48:50 +0800

    set JWT token for signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,24 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  // const newUser = await User.create(req.body);
+  // KEYNOTE: Prevent user register to be an Admin as we select info we need instead of let user input their role in the req.body
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: 'success',
+    token, //JWT token
     data: {
       user: newUser,
     },
