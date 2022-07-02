2b4e95301195a4d6e326761cca552e340c9c20c6
LIONSHI01 <shizijian2010@yahoo.com.hk>
2022-07-02 11:54:55 +0800

    add Aggregation pipeline for calculating AverageRating on Tours after creating reviews part1

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -50,6 +51,36 @@ reviewSchema.pre(/^find/, function (next) {
   });
 
   next();
+});
+
+// KEYNOTE:Add Aggregation Pipeline to calculate Average Rating on Tours after user creating reviews
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 }, //add 1 for each matcing tour
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  // Update Tour data
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating,
+  });
+};
+
+// Call the Aggregation Pipeline after new reviews saved
+// Use .post but not .pre, coz we calculate after saving the review data
+reviewSchema.post('save', function () {
+  // this points to current review
+  // NOTE: this.constructor = Review , coz 'this' point to the current review instance, its constructor is Review
+  this.constructor.calcAverageRatings(this.tour); // put in current review's tourId
 });
 
 const Review = mongoose.model('Review', reviewSchema);
