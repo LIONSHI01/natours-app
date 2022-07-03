36149e1f407a98ad9329e263d93b0e99119ec713
LIONSHI01 <shizijian2010@yahoo.com.hk>
2022-07-03 11:09:43 +0800

    create viewsRoutes and viewsController

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -70,9 +71,7 @@ app.use((req, res, next) => {
 
 // 2) ROUTES
 // These are Middlewares
-app.get('/', (req, res) => {
-  res.status(200).render('base');
-});
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
