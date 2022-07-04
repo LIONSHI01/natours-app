6fbff2ea2456b298774c216108e69b2bc5abdfc9
LIONSHI01 <shizijian2010@yahoo.com.hk>
2022-07-04 09:46:31 +0800

    implement mapbox

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -8,5 +8,32 @@ mapboxgl.accessToken =
   'pk.eyJ1IjoibXJsaW9uOTEiLCJhIjoiY2w1NWVzaW1yMTQwYzNpbjU2ejVoNW8wMCJ9.FCdfywynoFEVQU3KwzXoZg';
 var map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
+  style: 'mapbox://styles/mrlion91/cl561w442000014o69o3vz9sv',
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+  // Add marker
+  new mapboxgl.Marker({
+    elment: el,
+    ancor: 'bottom', //set ancor at the bottom of the marker image
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 200,
+    left: 100,
+    right: 200,
+  },
 });
