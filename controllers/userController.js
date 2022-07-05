ed6f5353e7b9b0fa22673fc16bbd3317d702edc0
LIONSHI01 <shizijian2010@yahoo.com.hk>
2022-07-05 18:20:42 +0800

    add sharp to corp uploaded img

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,25 @@
 const multer = require('multer');
-
+const sharp = require('sharp');
 const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
 // KEYNOTE: set up uploader as MIDDLEWARE
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/img/users');
-  },
-  filename: (req, file, cb) => {
-    //fileName format: user-userID-uploadTimestamp.jpeg
-    const ext = file.mimetype.split('/')[1]; //Get the file extension
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-  },
-});
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/img/users');
+//   },
+//   filename: (req, file, cb) => {
+//     //fileName format: user-userID-uploadTimestamp.jpeg
+//     const ext = file.mimetype.split('/')[1]; //Get the file extension
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//   },
+// });
+
+// Store image in memory buffer
+const multerStorage = multer.memoryStorage();
+
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
     cb(null, true);
@@ -29,6 +33,20 @@ const upload = multer({
   fileFilter: multerFilter,
 });
 exports.uploadUserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = (req, res, next) => {
+  if (!req.file) return next();
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  // 500*500 = square img
+  sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+};
 
 // For updateMe Route, filter out unwanted fields
 const filterObj = (obj, ...allowedFields) => {
